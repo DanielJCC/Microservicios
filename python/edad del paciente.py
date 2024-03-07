1709e1f2918aeed518e8cb2f246eb0dcd34a6d99
Juan Diaz G <39344961+Juan7diaz@users.noreply.github.com>
2024-03-07 15:04:16 -0500

    Delete python directory

--- python/edad del paciente.py
+++
@@ -1,9 +0,0 @@
-nombrecompleto = input("digite el nombre del paciente:")
-edaddigitada = input("digite la edad del paciente:")
-edad = int(edaddigitada)
-categoriaedad=None
-if edad>=18:
-    categoriaedad="Mayor de edad"
-else:
-    categoriaedad="Menor de edad"
-print("el paciente", nombrecompleto, "es" ,categoriaedad)
