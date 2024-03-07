ba6c0788883ebd67c724538f58281638766336c9
GMStaworld <Gianmarco.astori.p@gmai.com>
2024-03-07 18:20:30 -0500

    First message test

--- src/test/java/com/example/demo/repository/MensajeRepositoryTest.java
+++ src/test/java/com/example/demo/repository/MensajeRepositoryTest.java
@@ -4,6 +4,7 @@ package com.example.demo.repository;
 import java.time.LocalTime;
 import java.util.List;
 
+import com.example.demo.entities.Usuario;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,29 +16,43 @@ import com.example.demo.entities.Mensaje;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MensajeRepositoryTest extends AbstractIntegrationDBTest{
-    MensajeRepository mensajeRepository;
+    private MensajeRepository mensajeRepository;
+    private UsuarioRepository usuarioRepository;
 
     @Autowired
-    public MensajeRepositoryTest(MensajeRepository mensajeRepository) {
+    public MensajeRepositoryTest(MensajeRepository mensajeRepository, UsuarioRepository usuarioRepository) {
         this.mensajeRepository = mensajeRepository;
+        this.usuarioRepository = usuarioRepository;
     }
+
     @BeforeEach
     void setUp() {
+        usuarioRepository.deleteAll();
         mensajeRepository.deleteAll();
     }
     @Test
-    void givenAnMensaje_whenSave_thenMensajewithId(){
+    void givenNewMensaje_whenSave_thenMensajeIsPersisted(){
         //given
+        Usuario user = usuarioRepository.save(Usuario.builder()
+                .nombre("Juan")
+                .apellidos("Guerrero")
+                .userName("juanG")
+                .password("password123")
+                .build()
+        );
+
         Mensaje mensaje = Mensaje.builder()
-                .creador("Julian")
-                .destinatario("Cutupley")
+                .creador("Juan")
+                .destinatario("Marcelo")
                 .created_at(LocalTime.now())
-                .contenido("Hola Cutupley que tal estás")
+                .contenido("Agachate y conocelo")
+                .usuario(user)
                 .build();
         //when
         Mensaje messageSaved = mensajeRepository.save(mensaje);
+
         //then
         assertThat(messageSaved.getId()).isNotNull();
-
+        assertThat(messageSaved.getUsuario()).isEqualTo(user);
     }
 }
