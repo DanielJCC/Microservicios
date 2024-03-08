247879570149397894ede90652ff56a4d0b4ef67
GMStaworld <Gianmarco.astori.p@gmai.com>
2024-03-07 19:00:16 -0500

    Second test

--- src/test/java/com/example/demo/repository/MensajeRepositoryTest.java
+++ src/test/java/com/example/demo/repository/MensajeRepositoryTest.java
@@ -14,6 +14,7 @@ import com.example.demo.AbstractIntegrationDBTest;
 import com.example.demo.entities.Mensaje;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatList;
 
 public class MensajeRepositoryTest extends AbstractIntegrationDBTest{
     private MensajeRepository mensajeRepository;
@@ -31,6 +32,7 @@ public class MensajeRepositoryTest extends AbstractIntegrationDBTest{
         mensajeRepository.deleteAll();
     }
     @Test
+    @DisplayName("[CREATE] Dado un nuevo mensaje, cuando se crea, debe estar presente en la base de datos")
     void givenNewMensaje_whenSave_thenMensajeIsPersisted(){
         //given
         Usuario user = usuarioRepository.save(Usuario.builder()
@@ -54,5 +56,33 @@ public class MensajeRepositoryTest extends AbstractIntegrationDBTest{
         //then
         assertThat(messageSaved.getId()).isNotNull();
         assertThat(messageSaved.getUsuario()).isEqualTo(user);
+    }
+
+    @Test
+    @DisplayName("[READ] dado el nombre de un creador, los mensajes de dicho creador se pueden leer de la base de datos")
+    void givenCreador_whenFindByCreador_thenMensajeIsReturned(){
+        //given
+        Usuario user = usuarioRepository.save(Usuario.builder()
+                .nombre("Juan")
+                .apellidos("Guerrero")
+                .userName("juanG")
+                .password("password123")
+                .build()
+        );
+
+        Mensaje mensaje = Mensaje.builder()
+                .creador("Juan")
+                .destinatario("Marcelo")
+                .created_at(LocalTime.now())
+                .contenido("Agachate y conocelo")
+                .usuario(user)
+                .build();
+        Mensaje messageSaved = mensajeRepository.save(mensaje);
+        //when
+        List <Mensaje> mensajes=mensajeRepository.findByCreador(mensaje.getCreador());
+
+        //then
+        assertThat(mensajes).isNotNull();
+        assertThat(mensajes.get(0).getUsuario()).isEqualTo(user);
     }
 }
